1d38f108ce1e0663d90cd7a9c4b2c27992fc6aeb
David Chen <david.chen@fiscalnote.com>
2019-03-20 18:55:21 -0400

    create artifacts

--- devops/release.sh
+++ devops/release.sh
@@ -1,2 +1,9 @@
+cat >./artifacts/manifest.json <<EOF
+{
+    "artifactSchema": "default",
+    "version": "${CIRCLE_TAG}",
+    "commitHash": "${CIRCLE_SHA1}"
+}
+EOF
 bucket=davidchen
-aws s3 sync . s3://$bucket/packages/${CIRCLE_PROJECT_REPONAME}/artifacts/${CIRCLE_TAG}
+aws s3 sync ./artifacts s3://$bucket/packages/${CIRCLE_PROJECT_REPONAME}/artifacts/${CIRCLE_TAG}
