b984fda03e3a65ae6c42b003eb88e2f70dd74d33
David Chen <david.chen@fiscalnote.com>
2019-03-21 15:31:14 -0400

    version_info

--- devops/release.sh
+++ devops/release.sh
@@ -1,10 +1,11 @@
+version=`cat version_info`
 mkdir artifacts
 cat >./artifacts/manifest.json <<EOF
 {
     "artifactSchema": "default",
-    "version": "${CIRCLE_TAG}",
+    "version": "$version",
     "commitHash": "${CIRCLE_SHA1}"
 }
 EOF
 bucket=davidchen
-aws s3 sync ./artifacts s3://$bucket/packages/${CIRCLE_PROJECT_REPONAME}/artifacts/${CIRCLE_TAG}
+aws s3 sync ./artifacts s3://$bucket/packages/${CIRCLE_PROJECT_REPONAME}/artifacts/$version
