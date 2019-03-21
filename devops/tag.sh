813b8d9ca47b31608575d0ef5023bdc2a70ce90b
David Chen <david.chen@fiscalnote.com>
2019-03-21 16:25:44 -0400

    circleci

--- devops/tag.sh
+++ devops/tag.sh
@@ -1,4 +1,4 @@
 version=`cat version_info`
 git config --global user.email "<>"
 git config --global user.name "CircleCI"
-git tag -a $version ${CIRCLE_SHA1}
+git tag -a $version ${CIRCLE_SHA1} -m="Created by CircleCI"
